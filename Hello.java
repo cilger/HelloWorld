901324634c35f7b7bd3ef22aaab3680d999221d9
gerrit.cilliers <Gerrit.Cilliers@interfront.co.za>
2020-08-24 20:51:35 +0200

    added Hello World Change

--- Hello.java
+++ Hello.java
@@ -1,8 +1,13 @@
 public class Hello {
 
     public static void main(String[] args) {
-        for(int i = 1; i <= 10; i++){
-            System.out.println("Hello World ..." + i);
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 5) {
+                System.out.println("Hello from nr 5");
+            } else {
+                System.out.println("Hello World ..." + i);
+            }
         }
     }
 }
